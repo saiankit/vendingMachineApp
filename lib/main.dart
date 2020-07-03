2f2e4bea3235354348c642f438e528bd744cca4b
saiankit <saiankit30@gmail.com>
2020-07-03 20:20:15 +0530

    Created Home Screen for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vendingMachineApp/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,6 +11,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: HomeScreen(),
+      ),
+    );
   }
 }
